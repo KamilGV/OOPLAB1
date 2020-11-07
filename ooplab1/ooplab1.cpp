ab0acc81edb52a609f20d12f206ff2e76d0f3721
Камил Гильванов <gkamilv@gmail.com>
2020-11-07 16:46:03 +0500

    Добавлены класс, атрибуты, констркторы, деструкторы. Создание статических объектов.

--- ooplab1/ooplab1.cpp
+++ ooplab1/ooplab1.cpp
@@ -2,9 +2,48 @@
 //
 
 #include <iostream>
+#include <conio.h>
 
+
+class point
+{
+private:
+    int x;
+    int y;
+public:
+    point() {
+        printf("Конструктор\n");
+        x = 0;
+        y = 0;
+   }
+    point(int x, int y) {
+        printf("Конструктор с параметром\n");
+        this->x = x;
+        this->y = y;
+    }
+    point(const point& p) {
+        printf("Конструктор копирования\n");
+        x = p.x;
+        y = p.y;
+    }
+    ~point() {
+        printf("%d, %d\n", x, y);
+        printf("Деструктор\n");
+    }
+
+};
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+    {
+        printf("Статически созданные объкты: \n");
+        point p1;
+        point p2(1, 2);
+        point p3(p2);
+    }
+
     
+
+    _getch();
 }
 
